d372ae3297a8c550c48e8f0e14b3eae2d59f0f0b
hongdeshuai <hongdeshuai@gmail.com>
2020-02-06 08:37:21 -0800

    run the docker as background mode

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ ssh ubuntu@13.238.116.0 -tt <<EOF
 cd /home/ubuntu/resume
 git pull
 sudo docker-compose down
-sudo docker-compose up
+sudo docker-compose up -d
 echo "yet build in bash here"
 exit
 EOF
