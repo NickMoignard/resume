32e645729574c720f9422d55b657a6a079200062
hongdeshuai <hongdeshuai@gmail.com>
2020-02-06 07:21:29 -0800

    d

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
-ssh jenkins@13.238.116.0<<EOF
- cd /home/ubuntu/resume
- git pull
- sudo docker-compose down
- sudo docker-compose up
- exit
-EOF
+#!/bin/sh
+#  cd /home/ubuntu/resume
+#  git pull
+#  sudo docker-compose down
+#  sudo docker-compose up
+#  exit
+echo "yet build in bash here"
