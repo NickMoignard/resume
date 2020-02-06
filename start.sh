33d81bfc739e0c3f0b33997b1e46b1403be2ba58
hongdeshuai <hongdeshuai@gmail.com>
2020-02-06 07:10:17 -0800

    a

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 git pull
 sudo docker-compose down
-npm install
-npm start
+ssh jenkins@13.238.116.0<<EOF
+ cd /home/ubuntu/resume
+ git pull
+ sudo docker-compose down
+ sudo docker-compose up
+ exit
+EOF
