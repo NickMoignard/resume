bd7be6fc7f31a705e04e0f383e66ccca8c726fcf
hongdeshuai <hongdeshuai@gmail.com>
2020-02-06 08:43:46 -0800

    remove the containers when down

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 ssh ubuntu@13.238.116.0 -tt <<EOF
 cd /home/ubuntu/resume
 git pull
-sudo docker-compose down
+sudo docker-compose down --remove
 sudo docker-compose up -d
 echo "yet build in bash here"
 exit
