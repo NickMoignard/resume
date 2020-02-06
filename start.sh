0f9bc79fbb68908fc5ad6b7aa1de62dbc7347c73
hongdeshuai <hongdeshuai@gmail.com>
2020-02-06 09:00:38 -0800

    remove all old images built

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ cd /home/ubuntu/resume
 git pull
 sudo docker-compose down --remove
 sudo docker container prune -f
+sudo docker image rm $(sudo docker image ls)
 sudo docker-compose up -d
 echo "yet build in bash here"
 exit
