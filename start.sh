191f2c95b31f4a8bb2b698937c2a99d21d67463c
hongdeshuai <hongdeshuai@gmail.com>
2020-02-06 09:06:16 -0800

    only delete the latest image

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ cd /home/ubuntu/resume
 git pull
 sudo docker-compose down --remove
 sudo docker container prune -f
-sudo docker image rm $(sudo docker image ls)
+sudo docker image rm resume_frontend:latest
 sudo docker-compose up -d
 echo "yet build in bash here"
 exit
